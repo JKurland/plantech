557ab4d45f1ca747b0dfa4a5301cdac18c65d18b
Josh Kurland <josh.kurland@outlook.com>
2021-09-04 07:16:05 +0000

    click for shape

--- application/click_for_shape.h
+++ application/click_for_shape.h
@@ -11,17 +11,15 @@ namespace pt {
 class ClickForShape {
 public:
     EVENT(MouseButton) {
-        std::cout << event.x_rel << ", " << event.y_rel << std::endl;
         double x_rel = event.x_rel;
         double y_rel = event.y_rel;
 
         auto req = AddMesh{Mesh{{
             {{x_rel, y_rel-0.02}, {1.0f, 0.0f, 0.0f}},
-            {{x_rel+0.1, y_rel+0.01}, {0.0f, 1.0f, 0.0f}},
-            {{x_rel-0.1, y_rel+0.01}, {0.0f, 0.0f, 1.0f}},
+            {{x_rel+0.03, y_rel+0.01}, {0.0f, 1.0f, 0.0f}},
+            {{x_rel-0.03, y_rel+0.01}, {0.0f, 0.0f, 1.0f}},
         }}};
         co_await ctx(req);
-        co_return;
     }
 };
 
